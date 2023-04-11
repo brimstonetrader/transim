cb8425c7f0ec19fb11f7ade3f29ea35b7df5cf68
RhettBooher13 <rhettbooher@gmail.com>
2023-04-11 10:55:06 -0500

    Minor change to make pause button work.

--- Transim/Assets/Scripts/GameManager.cs
+++ Transim/Assets/Scripts/GameManager.cs
@@ -85,6 +85,7 @@ public class GameManager : MonoBehaviour
     
     }
     public void ChangeScene(string scene){
+        print("changing scene");
         StartCoroutine(LoadYourAsyncScene(scene));
     }
 
